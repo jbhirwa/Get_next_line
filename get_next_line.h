eaf377211bf577604d9a19e6f96766d6fe50bdfe
John Habaguhirwa <jhabaguh@F3C023.42adel.org.au>
2021-11-04 12:54:54 +1030

    passed

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhabaguh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 12:51:42 by jhabaguh          #+#    #+#             */
+/*   Updated: 2021/11/04 12:51:45 by jhabaguh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+
+#endif
